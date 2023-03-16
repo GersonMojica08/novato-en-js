8492cf0b61929bc3553deb090c7c3c5c0c8414c0
GersonMojica08 <76978073+GersonMojica08@users.noreply.github.com>
2023-03-15 23:07:58 -0600

    new bro

--- JS/main.js
+++ JS/main.js
@@ -51,7 +51,7 @@ div_1.classList.add("div_1");
 div_1.classList.add("esconder_elemento");
 const div_2 = document.createElement("div");
 div_2.classList.add("div_2");
-div_2.innerText = "Hola Bienvenido al Mundo Web";
+div_2.innerText = "Hola, Bienvenido al Mundo Web";
 
 const boton_eliminar_bienvenida = document.createElement("button");
 boton_eliminar_bienvenida.classList.add("boton_eliminar_bienvenida");
@@ -90,6 +90,7 @@ formulario_.classList.add("formularioclass");
 const inputName = document.createElement("input");
 inputName.classList.add("inputnameclass");
 inputName.placeholder = "Nombre";
+inputName.name = "nombre";
 
 const inputApellido = document.createElement("input");
 inputApellido.classList.add("inputapellidoclass");
@@ -102,39 +103,95 @@ inputcorreo.type ="email";
 
 const boton_guardar = document.createElement("button");
 boton_guardar.classList.add("boton_guardar");
-boton_guardar.innerHTML = "Guardar"
+boton_guardar.innerHTML = "Guardar";
 
 /* meter los input en el form */
 formulario_.appendChild(inputName);
 formulario_.appendChild(inputApellido);
 formulario_.appendChild(inputcorreo);
-formulario_.appendChild(boton_guardar);
+formulario_.appendChild(boton_guardar);/** modificacion */
 
 /* evento para mostrar el formulario en el html al dar clic
 en el acapite formulario*/
 body.appendChild(formulario_);
+
 etiqueta_li2.addEventListener("click", function() {
   formulario_.classList.add("formularioclass_active")
 })
+
 /*  Quitar lo que esta en li2 cuando das clic en li1 */
 etiqueta_li1.addEventListener("click", function() {
   formulario_.classList.remove("formularioclass_active");
 })
+
 /*  Quitar lo que esta en li1 cuando das clic en li2 */
 etiqueta_li2.addEventListener("click", function() {
   greeatingButton.classList.remove("greeting_button_active");
   div_1.classList.add("esconder_elemento");
 
 })
+
 /*********************************************************************/
 etiqueta_li1.addEventListener("click", function() {
   div_1.classList.add("div_1");
 })
+
  // borrar la bienvenida
 boton_eliminar_bienvenida.addEventListener("click", function() {
   div_1.classList.add("esconder_elemento");
 })
+
 /*********************************************************************/
+/*                 ACA SE LE DARA VIDA AL FORMULARIO                 */
+/*********************************************************************/
+var PRINCIPAL = []
+boton_guardar.addEventListener("click", function(e) {
+  e.preventDefault();
+  const captura_data = formulario_.querySelectorAll("input");
+  console.log("este un clic de prueva",captura_data);
+  agregar_a_tabla(captura_data);
+  
+})
 
 
+function actualizar_tabla(){
+  let tbody = document.getElementById("datos");
+  tbody.innerHTML = "";
+
+  for(let i=0; i < PRINCIPAL.length; i++){
+      let trt = "<tr ><td>"+ PRINCIPAL[i].nombre +"</td><td>"+ PRINCIPAL[i].apellido  +"</td><td>"+ PRINCIPAL[i].correo +"</td><td>"+ "<button onclick=eliminar('"+ i +"')>X</button>" +"</td></tr>"
+       tbody.innerHTML+=trt;
+   }
+}
+
+/*  */
+
+function agregar_a_tabla(captura_data){
+    //console.log(formulario);
+    let linea = {
+      nombre: captura_data[0].value,
+      apellido: captura_data[1].value,
+      correo: captura_data[2].value
+    }
+    PRINCIPAL.push(linea);
+    actualizar_tabla();
+    return false;
+}
+
+function eliminar(i){
+  delete PRINCIPAL[i];
+  actualizar_tabla();
+}
+
+/*esto es para esconder y aparecer la tabla*/
+const tabla_data = document.getElementById("tabla");
+tabla_data.classList.add("esconder_elemento")
+
+etiqueta_li1.addEventListener("click", function() {
+  tabla_data.classList.add("esconder_elemento");
+})
+
+etiqueta_li2.addEventListener("click", function() {
+  tabla_data.classList.remove("esconder_elemento");
+})
 
